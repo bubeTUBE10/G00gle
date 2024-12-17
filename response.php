4fb3cf4393008a3b975d93250a736a8cb66bc09d
bubeTUBE10 <rubenpaolini@mailfence.com>
2024-12-17 11:45:59 -0500

    sa

--- response.php
+++ response.php
@@ -6,7 +6,6 @@
     <?php
         $search = htmlspecialchars($_GET["q"]);
         $_SERVER['REMOTE_ADDR'];
-        $ip = var_export($_SERVER, true);
         $server = "localhost";
         $username = "php";
         $password = "hello";
@@ -20,7 +19,7 @@
 
         // Prepare and execute the SQL query
         //$sql = "INSERT INTO car_brands(company) VALUES ('$brand');";  // Ensure the variable is properly quoted
-        $sql = "INSERT INTO google(ip, search) VALUES ('$ip', '$search');";
+        $sql = "INSERT INTO google(ip, search) VALUES ('$_SERVER', '$search');";
         $result = mysqli_query($conn, $sql);
     ?>
 </head>
@@ -28,7 +27,7 @@
 <body>
 
 You searched <?php echo $_GET["q"]; ?><br>
-Your IP is <?php echo $ip; ?><br>
+Your IP is <?php echo $_SERVER; ?><br>
 
 </body>
 </html>
