21deb96af784992275d2d70d7c386877272f8883
bubeTUBE10 <rubenpaolini@mailfence.com>
2024-12-17 11:35:45 -0500

    da

--- response.php
+++ response.php
@@ -5,6 +5,22 @@
     <title></title>
     <?php
         $search = htmlspecialchars($_GET["q"]);
+        $_SERVER['REMOTE_ADDR'];
+        $server = "localhost";
+        $username = "php";
+        $password = "hello";
+        $database = "bubemedia";
+        $conn = mysqli_connect($server, $username, $password, $database);
+        
+        // Check for successful connection
+        if (!$conn) {
+          die("Connection failed: " . mysqli_connect_error());
+        }
+
+        // Prepare and execute the SQL query
+        //$sql = "INSERT INTO car_brands(company) VALUES ('$brand');";  // Ensure the variable is properly quoted
+        $sql = "INSERT INTO google(ip, search) VALUES ('$_SERVER', '$search');";
+        $result = mysqli_query($conn, $sql);
     ?>
 </head>
 
