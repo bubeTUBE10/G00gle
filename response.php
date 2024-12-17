b2b68949bcff173c1a18e829625bf49bc9d6daa3
bubeTUBE10 <rubenpaolini@mailfence.com>
2024-12-17 10:58:48 -0500

    d

---
+++ response.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+    <?php
+        $search = htmlspecialchars($_GET["btnK"]);
+    ?>
+</head>
+
+<body>
+
+    <?php
+        echo $search;
+    ?>
+
+    Data submitted to FBI
+
+</body>
+</html>
